977374afc51ad34f86e481b89a1b7d6e2414b193
marco scarpa <65711225+mrshu1978@users.noreply.github.com>
2026-02-28 13:58:45 +0100

    Initial scaffold (12 files)

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Project Ready for Development" />
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
